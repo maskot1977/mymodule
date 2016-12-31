c0411cd8cbe2fb9d29db013dc14b280372089ea3
Mas Kot <maskot1977@gmail.com>
2016-12-31 22:47:51 +0900

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from myplot import myplot
 from removeControlCharacter import removeControlCharacter
 from itermLog import itermLog
+from get_col_name_frm_gz import get_col_name_frm_gz
 
--- get_row_name_frm_gz.py
+++ get_row_name_frm_gz.py
@@ -2,14 +2,13 @@ import gzip
 import sys
 import pyximport; pyximport.install(pyimport = True)
 
-def get_col_name_frm_gz(file, head=10):
+def get_row_name_frm_gz(file, head=10):
    ret = []
    for i, line in enumerate(gzip.open(file)):
-      ret = line.strip().split("\t")
-      break
+      ret.append(line.split("\t")[0])
    return ret
 
 if __name__ == '__main__':
    args = sys.argv
    file = args[1]
-   print get_col_name_frm_gz(file)
+   print get_row_name_frm_gz(file)
